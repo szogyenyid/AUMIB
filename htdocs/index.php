c2e335b2a3382950cdbe05f20b6ccf54e1f6c8d2
Unknown <szogyenyid@gmail.com>
2018-04-26 19:29:09 +0200

    Added list of categories

--- htdocs/index.php
+++ htdocs/index.php
@@ -3,13 +3,21 @@
 		<title>AUMIB</title>
 	</head>
 	<body>
-	<!-- PHP to load same header -->
+	<script src="konami.js"></script>
+	<!-- PHP to load contant header -->
 	<!-- Banner -->
 	<!-- List of boards in table, with hyperlink -->
+	<?php
+	$link = mysqli_connect("localhost","root","","aumib");
+	if(!$link){
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+		exit;
+	}
+	else echo "Connection successful";
+	?>
 	<!-- Stats: total posts, total images -->
-	<!--  -->
-	<!--  -->
-	<!--  -->
-	<!-- PHP to load same footer -->
+	<!-- PHP to load constant footer -->
 	</body>
 </html>
