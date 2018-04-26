f5aeac9d70f4ece031b86f7553164ec26e7dbcb2
Unknown <szogyenyid@gmail.com>
2018-04-26 13:04:17 +0200

    added empty index.php with todo

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>AUMIB</title>
+	</head>
+	<body>
+	<!-- PHP to load same header -->
+	<!-- Banner -->
+	<!-- List of boards in table, with hyperlink -->
+	<!-- Stats: total posts, total images -->
+	<!--  -->
+	<!--  -->
+	<!--  -->
+	<!-- PHP to load same footer -->
+	</body>
+</html>
