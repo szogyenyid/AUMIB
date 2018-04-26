36dd680026bfc466f79e3eb0cec81553196091d8
Unknown <szogyenyid@gmail.com>
2018-04-26 22:13:23 +0200

    list of categories in index

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,22 +1,33 @@
 <html>
 	<head>
 		<title>AUMIB</title>
+		<link rel="stylesheet" type="text/css" href="theme.css">
+		<script src="konami.js"></script>
 	</head>
+<!--------------------------------------------------------------------------------->
 	<body>
-	<script src="konami.js"></script>
+	
 	<!-- PHP to load contant header -->
 	<!-- Banner -->
 	<!-- List of boards in table, with hyperlink -->
 	<?php
-	$link = mysqli_connect("localhost","root","","aumib");
-	if(!$link){
-		echo "Error: Unable to connect to MySQL." . PHP_EOL;
-		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-		exit;
-	}
-	else echo "Connection successful";
+	include 'connect.php';
+	$link = getDB();
+	$cats=mysqli_query($link, "SELECT * from categories Group by Id");
 	?>
+	<center><h2>Categories</h2></center>
+	<center><table class="index">
+		<th class="index">Abbr</th>
+		<th class="index">Name</th>
+		<th class="index">Description</th>
+	<?php while ($row = mysqli_fetch_array($cats)): ?>
+		<tr class="index">
+			<td class="index"><?= $row['Id'] ?></td>
+			<td class="index"><?= $row['Name'] ?></td>
+			<td class="index"><?= $row['Description'] ?></td>
+		</tr>
+	<?php endwhile; ?>
+	</table><center>
 	<!-- Stats: total posts, total images -->
 	<!-- PHP to load constant footer -->
 	</body>
