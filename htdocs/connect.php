549bfa2aa242dc44e4a08048403280be52f1086a
Unknown <szogyenyid@gmail.com>
2018-04-26 22:12:51 +0200

    added connect.php

---
+++ htdocs/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+function getDB(){
+	$link = mysqli_connect("localhost","root","","aumib");
+	if(!$link){
+		echo "Error: Unable to connect to MySQL." . PHP_EOL;
+		echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+		echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+		exit;
+	}
+	mysqli_query($link, "set character_set_results='utf8'");
+	return $link;
+}
+
+?>
