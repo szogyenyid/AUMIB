a6db48299985746a4cc96f7e80813e84527e63dc
Unknown <szogyenyid@gmail.com>
2018-04-27 12:44:12 +0200

    Added "add category" to admin panel

--- htdocs/nigoLnimda.php
+++ htdocs/nigoLnimda.php
@@ -1,11 +1,30 @@
 <html>
-
-	<head>
-		<title>AUMIB Admin</title>
-	</head>
+<head>
+	<title>AUMIB Admin</title>
+</head>
 <!-------------------------------------------------------------->
-	<body>
-		<h1>Admin login</h1>
-	</body>
+<body>
+	<h1>Admin's control panel</h1>
+	<h3>Add new category<h3>
+	<form action="nigoLnimda.php" method="post">
+		Abbr: <input type="text" name="abbr"><br>
+		Name: <input type="text" name="name"><br>
+		Desc: <input type="text" name="desc"><br>
+		<input type="submit" value="Add" name="newCat">
+	</form>
+	<br>
 	
+</body>
 </html>
+
+<?php
+	if(isset($_POST['newCat'])){
+		include 'connect.php';
+		$link = getDB();
+		$query = "INSERT INTO categories VALUES ('" . mysqli_real_escape_string($link, $_POST['abbr']) . "','" . mysqli_real_escape_string($link, $_POST['name']) . "','" . mysqli_real_escape_string($link, $_POST['desc']) . "');";
+		echo $query;
+		mysqli_query($link, $query);
+		mysqli_close($link);		
+	}
+
+?>
