009e101a16c64873cb2f414c2ca820ab1de845a7
Unknown <szogyenyid@gmail.com>
2018-04-27 13:09:22 +0200

    removed test echo

--- htdocs/nigoLnimda.php
+++ htdocs/nigoLnimda.php
@@ -22,7 +22,7 @@
 		include 'connect.php';
 		$link = getDB();
 		$query = "INSERT INTO categories VALUES ('" . mysqli_real_escape_string($link, $_POST['abbr']) . "','" . mysqli_real_escape_string($link, $_POST['name']) . "','" . mysqli_real_escape_string($link, $_POST['desc']) . "');";
-		echo $query;
+		//echo $query;
 		mysqli_query($link, $query);
 		mysqli_close($link);		
 	}
