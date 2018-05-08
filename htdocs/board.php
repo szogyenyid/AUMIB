1cef2991db4cd519fd49a729979d323486e7bd76
Unknown <szogyenyid@gmail.com>
2018-05-08 14:39:33 +0200

    Started board.php

--- htdocs/board.php
+++ htdocs/board.php
@@ -3,13 +3,27 @@
 		include 'connect.php';
 		$link = getDB();
 		$category=mysqli_real_escape_string($link, $_GET['cat']);
-		//echo $category;
+		$query = "SELECT * FROM categories WHERE " . "Id='" . $category . "';";
+		//echo $query;
+		$catName = mysqli_query($link, $query);
+		$row = mysqli_fetch_array($catName);
+		$catName = $row['Name'];
 	}
+?>
+
+<?php
+	$query = "SELECT * FROM threads WHERE CatId='" . $category . "' Group by Datetime;";
+	//echo $query;
+	$threads=mysqli_query($link, $query);
 ?>
 
 <html>
 	<head>
+		<title><?= $catName?> - AUMIB</title>
 	</head>
 	<body>
+		<!-- Start new thread -->
+		<!-- List threads -->
+		
 	</body>
 </html>
