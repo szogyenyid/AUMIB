f3e69b504826732ba4d27f536bc25ad5ab0387d2
Unknown <szogyenyid@gmail.com>
2018-05-12 17:07:43 +0200

    Created static footer

---
+++ htdocs/footer.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+	</head>
+	<body>
+	<br><br>
+	<center>
+	<font size="1">
+	AUMIB - Anonymous UnModerated ImageBoard - Created by szogyenyid - 2018 <br> hw for VIAUAB01 @ Budapest University of Technology and Economics
+	</font>
+	</center>
+	</body>
+</html>
