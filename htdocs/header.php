01a5aa7b5521edc4dda80d3eb3e46508f650a9f9
Unknown <szogyenyid@gmail.com>
2018-05-12 17:07:33 +0200

    Created header to link all categories

---
+++ htdocs/header.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+	</head>
+	<body>
+	<?php
+	include 'connect.php';
+	$link = getDB();
+	$catIds=mysqli_query($link, "SELECT Id from categories GROUP BY Id ASC;");
+	?>
+	<center>
+	[ <a href = "index.php">Home</a>
+	<?php while($row = mysqli_fetch_array($catIds)): ?>
+		 /<a href="board.php?cat=<?= $row['Id']?>"> <?=$row['Id']?></a>
+	<?php endwhile; ?>]
+	</center>
+	</body>
+</html>
+<?php
+	mysqli_close($link);
+?>
