75d3dde089a9e237d6dda16341fd38185da1b3ee
Unknown <szogyenyid@gmail.com>
2018-05-12 12:54:31 +0200

    corrected test data

--- init_test_data.sql
+++ init_test_data.sql
@@ -10,5 +10,6 @@ INSERT INTO threads VALUES(2,'Best pens', 'Hi, What kind of pens do u use for ta
 INSERT INTO threads VALUES(3,'Python multithreading projects', 'Hey! Any ideas on beginner projects for python multithreading? thx', '', now(), 'pr');
 
 INSERT INTO replies VALUES(1, 'Hey, I use Zebra Super Fine H-8000 0.5 from my local shop, and i love it', '', now(), 2);
-INSERT INTO replies VALUES(2, 'relpy', 'python sucks, only indians play with snakes', now(), 3);
-INSERT INTO replies VALUES(3, 'reply', 'Hi, I just started multithreading, and I am writing a ', now(), 3);
+INSERT INTO replies VALUES(2, 'python sucks, only indians play with snakes', '', now(), 3);
+INSERT INTO replies VALUES(3, 'Hi, I just started multithreading, and I am writing an IRC client', '', now(), 3);
+
