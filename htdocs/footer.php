6c385446cf8626bf7018709298674dc48df2cba6
Unknown <szogyenyid@gmail.com>
2018-05-14 19:58:45 +0200

    footer set at bottom of page

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,12 +1,15 @@
 <html>
 	<head>
+		<link rel="icon" href="favicon.ico" type="image/ico">
 	</head>
 	<body>
-	<br><br>
-	<center>
-	<font size="1">
-	AUMIB - Anonymous UnModerated ImageBoard - Created by <a href="https://github.com/szogyenyid">szogyenyid</a> - 2018 <br> hw for VIAUAB01 @ Budapest University of Technology and Economics
-	</font>
-	</center>
+	<div id="footerbody">
+		<br><br>
+		<center>
+		<font size="1">
+		AUMIB - Anonymous UnModerated ImageBoard - Created by <a href="https://github.com/szogyenyid">szogyenyid</a> - 2018 <br> hw for VIAUAB01 @ Budapest University of Technology and Economics
+		</font>
+		</center>
+	</div>
 	</body>
 </html>
