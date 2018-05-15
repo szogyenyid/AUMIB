1d98da6ab4ee1f99c3a3aed09ace8b1fe4ef158d
Unknown <szogyenyid@gmail.com>
2018-05-15 08:54:05 +0200

    search.php done

--- htdocs/search.php
+++ htdocs/search.php
@@ -41,10 +41,69 @@
 					$query2 = $query2."SELECT * from replies WHERE Reply Like '%".mysqli_real_escape_string($link, $_GET['text'])."%';";
 				endif;
 			}
-			echo $query1;
-			echo $query2;
+			//echo $query1;
+			//echo $query2;
+			$replies;
+			if($query2 != "") $replies = mysqli_query($link, $query2);
 		?>
-		
+		<div id="subtitle">Threads</div>
+		<div id="sThrTable">
+			<div id="sThrTableRow">
+				<div id="sThrTableHeader">Id</div>
+				<div id="sThrTableHeader">Title</div>
+				<div id="sThrTableHeader">Post</div>
+				<div id="sThrTableHeader">Link</div>
+				<div id="sThrTableHeader">Datetime</div>
+				<div id="sThrTableHeader">Category</div>
+			</div>
+			<?php if($query1 != ""): ?>
+				<?php $threads = mysqli_query($link, $query1); ?>
+				<?php while ($row = mysqli_fetch_array($threads)): ?>
+					<div id="sThrTableShortCell"><?=$row['Id'] ?></div>
+					<div id="sThrTableCell"><?=$row['Title'] ?></div>
+					<div id="sThrTableCell"><?=$row['Post'] ?></div>
+					<?php if(($row['Image']) == ""): ?>
+						<div id="sThrTableShortCell">No link</div>
+					<?php else: ?>
+					<?php if (strpos($row['Image'], 'http') === false) {
+						$row['Image'] = 'http://' .$row['Image'];
+}					?>
+					<div id="sThrTableShortCell"><a target="_blank" href="<?=$row['Image']?>">LINK</a></div>
+					<?php endif; ?>
+					<div id="sThrTableShortCell"><?=$row['Datetime'] ?></div>
+					<div id="sThrTableShortCell"><?=$row['CatId'] ?></div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+		<br>
+		<br>
+		<div id="subtitle">Replies</div>
+		<div id="sThrTable">
+			<div id="sThrTableRow">
+				<div id="sThrTableHeader">Id</div>
+				<div id="sThrTableHeader">Reply</div>
+				<div id="sThrTableHeader">Link</div>
+				<div id="sThrTableHeader">Datetime</div>
+				<div id="sThrTableHeader">Thread</div>
+			</div>
+			<?php if($query2 != ""): ?>
+				<?php $replies = mysqli_query($link, $query2); ?>
+				<?php while ($row = mysqli_fetch_array($replies)): ?>
+					<div id="sThrTableShortCell"><?=$row['Id'] ?></div>
+					<div id="sThrTableCell"><?=$row['Reply'] ?></div>
+					<?php if(($row['Image']) == ""): ?>
+						<div id="sThrTableShortCell">No link</div>
+					<?php else: ?>
+					<?php if (strpos($row['Image'], 'http') === false) {
+						$row['Image'] = 'http://' .$row['Image'];
+}					?>
+					<div id="sThrTableShortCell"><a target="_blank" href="<?=$row['Image']?>">LINK</a></div>
+					<?php endif; ?>
+					<div id="sThrTableShortCell"><?=$row['Datetime'] ?></div>
+					<div id="sThrTableShortCell"><?=$row['ThrId'] ?></div>
+				<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
 	</div>
 	<?php include ("footer.php") ?>
 	</body>
