ca8d7d8eb1f76a833672a1a5c8954bb7be54819d
Unknown <szogyenyid@gmail.com>
2018-05-15 08:57:00 +0200

    footer github link opens in new tab

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -7,7 +7,7 @@
 		<br><br>
 		<center>
 		<font size="1">
-		AUMIB - Anonymous UnModerated ImageBoard - Created by <a href="https://github.com/szogyenyid">szogyenyid</a> - 2018 <br> hw for VIAUAB01 @ Budapest University of Technology and Economics
+		AUMIB - Anonymous UnModerated ImageBoard - Created by <a href="https://github.com/szogyenyid" target="_blank" >szogyenyid</a> - 2018 <br> hw for VIAUAB01 @ Budapest University of Technology and Economics
 		</font>
 		</center>
 	</div>
